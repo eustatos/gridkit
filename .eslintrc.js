d94cfc2fe7b0e0396eb578da9e466814e28dfb4d
eustatos <astashkinav@gmail.com>
2026-01-24 22:56:57 +0400

    fix: typo packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,31 @@
 module.exports = {
   root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint'
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   env: {
+    browser: true,
     node: true,
-    es2021: true
+    es2021: true,
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       parserOptions: {
-        project: ['./tsconfig.json'],
+        project: ["./tsconfig.json"],
         tsconfigRootDir: __dirname,
       },
     },
     {
-      files: ['packages/core/**/*.ts'],
+      files: ["packages/core/**/*.ts"],
       rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
       },
     },
   ],
-  ignorePatterns: ['dist/', 'build/', 'node_modules/'],
+  ignorePatterns: ["dist/", "build/", "node_modules/"],
   rules: {
     // Добавьте сюда ваши кастомные правила
-  }
+  },
 };
